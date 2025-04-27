568f2bd5a7178b60539261119dd88b1b5ef4ec49
Moumine5578 <moumine.kone@epsi.fr>
2025-04-27 19:15:22 +0200

    Mise a jour du front, Accueil, a-propos etc...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StatistiquesComponent } from './statistiques/statistiques.component'; // ✅ chemin à adapter
+import { AcceuilComponent } from './Home/acceuil/acceuil.component';
+import { HeaderComponent } from "./Header/header/header.component";
+import { FooterComponent } from './Footer/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StatistiquesComponent],
+  imports: [RouterOutlet, HeaderComponent,FooterComponent],
+  standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AcceuilComponent } from './Home/acceuil/acceuil.component';
+import { StatistiquesComponent } from './statistiques/statistiques.component';
+import { AProposComponent } from './A-propos/a-propos/a-propos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', component: AcceuilComponent},
+   { path:'accueil', component: AcceuilComponent},
+   { path: 'tableau-de-bord', component: StatistiquesComponent},
+   { path: 'a-propos', component: AProposComponent}
+];
